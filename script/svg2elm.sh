41d54389cbc235e158f678418e406ad4e2c341e4
MATSUBARA Nobutada <nobutada0507@gmail.com>
2022-12-01 00:02:01 +0900

    CI: fix add STACK_BUILD_OPTS to exec

--- script/svg2elm.sh
+++ script/svg2elm.sh
@@ -3,7 +3,7 @@ set -eux
 
 cd svg2elm
 stack build $(echo $STACK_BUILD_OPTS)
-stack exec -- svg2elm ../octicons/icons/ ../src
+stack exec $(echo $STACK_BUILD_OPTS) -- svg2elm ../octicons/icons/ ../src
 cd ../
 elm-format --yes src/Octicons/
 elm make
